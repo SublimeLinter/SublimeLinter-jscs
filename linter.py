a13ade0e16b4137307435b650700c12aa2c400b7
Aparajita Fishman <aparajita@aparajita.com>
2014-01-24 16:28:03 +0200

    Better syntax_map by default, only need 'html' syntax.

--- linter.py
+++ linter.py
@@ -17,7 +17,7 @@ class Jscs(Linter):
 
     """Provides an interface to jscs."""
 
-    syntax = ('javascript', 'html', 'html 5')
+    syntax = ('javascript', 'html')
     cmd = 'jscs -r checkstyle'
     version_args = '--version'
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
